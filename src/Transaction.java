b452523bc2e3af4b9c084d8003293c52bf871847
jay-ui12 <jayandrewclamares@gmail.com>
2026-03-18 08:32:16 +0900

    refactor: show balance after withdraw

--- src/Transaction.java
+++ src/Transaction.java
@@ -9,6 +9,7 @@ public class Transaction {
         if (amount <= user.getBalance()) {
             user.setBalance(user.getBalance() - amount);
             System.out.println("Withdraw successful!");
+            System.out.println("Balance: " + user.getBalance());
         } else {
             System.out.println("Insufficient balance.");
         }
